9cba3037eb4021e0d356b57a28d1491bf811157b
Habeeb-M <habeebmohammed2005@gmail.com>
2019-02-14 16:37:19 +0000

    13+14/02/19
    block, ingot and item stuff

--- src/main/java/com/rustik/rustikssecondmod.java
+++ src/main/java/com/rustik/rustikssecondmod.java
@@ -1,32 +1,50 @@
 package com.rustik;
 
+import com.rustik.blocks.ModBlocks;
+import com.rustik.items.ModItems;
+import com.rustik.proxy.CommonProxy;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = rustikssecondmod.modId, name = rustikssecondmod.name, version = rustikssecondmod.version)
 public class rustikssecondmod {
+
     public static final String modId = "rustikssecondmod";
     public static final String name = "Rustik's Second Mod";
     public static final String version = "1.0.0";
 
     @Mod.Instance(modId)
     public static rustikssecondmod instance;
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {}
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {}
 
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        System.out.println(name + " is loading!");
+    public void postInit(FMLPostInitializationEvent event) {}
+
+    @SidedProxy(serverSide = "com.rustik.proxy.CommonProxy", clientSide = "com.rustik.proxy.ClientProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventBusSubscriber
+    public static class RegistrationHandler {
+        @SubscribeEvent
+        public static void registerItems(RegistryEvent.Register<Item> event) {
+            ModItems.register(event.getRegistry());
+            ModBlocks.registerItemBlocks(event.getRegistry());
+        }
+        @SubscribeEvent
+        public static void registerModels(ModelRegistryEvent event) {
+            ModItems.registerModels();
+            ModBlocks.registerModels();
+        }
     }
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-
-    }
-
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-
-    }
-
 }
+
