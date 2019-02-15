03baa8a0dfdbd80689ade0595fede3e1b7297349
Habeeb-M <habeebmohammed2005@gmail.com>
2019-02-15 20:55:44 +0000

    14/02/19

--- src/main/java/com/rustik/blocks/ModBlocks.java
+++ src/main/java/com/rustik/blocks/ModBlocks.java
@@ -1,21 +1,28 @@
 package com.rustik.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModBlocks {
 
-    public static void register(IForgeRegistry<Block> registry) {
+    public static BlockOre oreCopper = new BlockOre("ore_copper").setCreativeTab( CreativeTabs.MATERIALS);
 
+    public static void register(IForgeRegistry<Block> registry) {
+        registry.registerAll(
+                oreCopper
+        );
     }
 
     public static void registerItemBlocks(IForgeRegistry<Item> registry) {
-
+        registry.registerAll(
+                oreCopper.createItemBlock()
+        );
     }
 
     public static void registerModels() {
-
+        oreCopper.registerItemModel(Item.getItemFromBlock(oreCopper));
     }
 
 }
